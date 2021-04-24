6bbecaf55c07f96becabd10aef4398ece853d983
Carl Waddingham <craldev@gmail.com>
2021-04-24 20:03:08 +0100

    Under da sea

---
+++ LD48_Unity/Assets/SkyboxController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private List<DepthValue> depthValues = new List<DepthValue>();
+
+    [SerializeField]
+    private int currentIndex = 0;
+
+    [SerializeField]
+    private float currentDepth;
+
+    [SerializeField]
+    private Vector2 playerDepthRange;
+
+    private void Start()
+    {
+        playerDepthRange.x = depthValues[0].depth;
+        playerDepthRange.y = depthValues[depthValues.Count - 1].depth;
+    }
+
+    private void Update()
+    {
+        var nextIndex = Mathf.Clamp(currentIndex + 1, 0, depthValues.Count - 1);
+
+        if (depthValues[currentIndex].depth < player.transform.position.y)
+        {
+            currentIndex--;
+        }
+        else if (depthValues[nextIndex].depth > player.transform.position.y)
+        {
+            currentIndex++;
+        }
+        currentIndex = Mathf.Clamp(currentIndex, 0, depthValues.Count - 1);
+
+        currentDepth = Mathf.InverseLerp(depthValues[currentIndex].depth, depthValues[nextIndex].depth, player.transform.position.y);
+
+        var cameraBackgroundColor = Color.Lerp(depthValues[currentIndex].color, depthValues[nextIndex].color, currentDepth % 1);
+        camera.backgroundColor = cameraBackgroundColor;
+        RenderSettings.fogColor = cameraBackgroundColor;
+
+        RenderSettings.fogDensity = Mathf.Lerp(depthValues[currentIndex].fogDensity, depthValues[nextIndex].fogDensity, currentDepth % 1);
+    }
+}
+
+[Serializable]
+public struct DepthValue
+{
+    public Color color;
+    public float fogDensity;
+    public float depth;
+}
