eafcdd7ff7c42fdd3fe0a87285c636629a229b7b
Carl Waddingham <craldev@gmail.com>
2021-04-24 12:08:15 +0100

    Package Setup

---
+++ LD48_Unity/Assets/Game/Scripts/Gameplay/Player/CharacterController.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace LD48.Gameplay.Player
+{
+    public class CharacterController : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody rigidbody;
+
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
